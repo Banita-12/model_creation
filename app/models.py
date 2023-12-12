49a7f23bfd4420a42fda555d18d94ba02df4c4ff
Banita-12 <banitasamantara023@gmail.com>
2023-12-12 18:18:26 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class WebPage(models.Model):
     topic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
     url=models.URLField()
+    email=models.EmailField(default='abc@gmail.com')
 
     def __str__(self):
         return self.name
